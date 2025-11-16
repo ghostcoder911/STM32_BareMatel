61136cac5887ec79bd43254f29c12f5e5fd7df7b
Mathan MG <61845209+myt-mthn@users.noreply.github.com>
2025-11-16 09:27:43 +0530

    uploaded

--- src/main.c
+++ src/main.c
@@ -1,1 +1,17 @@
 
+#include "stm32f103xx.h"
+
+void delay(int t){
+	for (int i = t *8000 ;  i > 0; i--);
+}
+
+int main(void)
+{
+	GPIOA_CLK_EN();
+	GPIOA->CRL &= ~(0xF << (1 * 4));
+	GPIOA->CRL |=  (0x2 << (1 * 4));
+	while(1){
+		GPIOA->ODR ^= (1 << 1);
+		delay(200);
+	}
+}
